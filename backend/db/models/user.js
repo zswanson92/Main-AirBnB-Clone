af5615e54416e06d10889a21b8f20b26e94cde0a
zswanson92 <zackta92@gmail.com>
2022-10-20 16:33:15 -0500

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,14 +1,49 @@
 'use strict';
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    toSafeObject(){
+      const { id, username, email} = this;
+      return { id, username, email};
+    }
+    validatePassword(password){
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    static getCurrentUserById(id){
+      return User.scope('currentUser').findByPk(id);
+    }
+    static async login({ credential, password }){
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if(user && user.validatePassword(password)){
+        return await User.scope('currentUser').findByPk(user.id)
+      }
+    }
+    static async signup({ username, email, password }){
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id)
+    }
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
     static associate(models) {
       // define association here
     }
@@ -44,6 +79,19 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ['hashedPassword'] }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
   });
   return User;
 };
