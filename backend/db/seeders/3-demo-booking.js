a9c9d8e700174ee6e5a91a6cff000a632c2fcb42
zswanson92 <zackta92@gmail.com>
2022-10-24 17:18:00 -0500

    attempt at another timestamp fix

--- backend/db/seeders/3-demo-booking.js
+++ backend/db/seeders/3-demo-booking.js
@@ -16,20 +16,20 @@ module.exports = {
       {
         spotId: 1,
         userId: 1,
-        startDate: 2022/10/30,
-        endDate: 2022/11/11
+        startDate: '2022-10-30T16:32:02',
+        endDate: '2022-11-11T16:37:02'
       },
       {
         spotId: 2,
         userId: 2,
-        startDate: 2022-11-30,
-        endDate: 2022/12/11
+        startDate: '2022-11-30T16:34:02',
+        endDate: '2022-12-11T16:39:02'
       },
       {
         spotId: 3,
         userId: 3,
-        startDate: 2022-12-30,
-        endDate: 2022-12-31
+        startDate: '2022-12-30T16:31:02',
+        endDate: '2022-12-31T16:33:02'
       }
     ], {})
   },
