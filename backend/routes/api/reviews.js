7cc4dc7962268bf7fa5c32881d86e1f9cfc2a267
zswanson92 <zackta92@gmail.com>
2022-10-26 19:16:38 -0500

    end of weds trash commit

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -68,7 +68,80 @@ router.get('/current', restoreUser, requireAuth, async (req, res) => {
     res.json({"Reviews": emptyArr})
 }) // seems to be working on local
 
+
 router.post('/:reviewId/images', requireAuth, async (req, res) => {
+    const { reviewId } = req.params
+    let theRevieww = await Review.findByPk(reviewId)
+    const theSpot = await Spot.findByPk(theRevieww.spotId)
+    const { url } = req.body
+    const { user } = req
+    const userId = user.toSafeObject().id
+    let theReview = await Review.findAll({
+        where: {  spotId: theSpot.dataValues.id, userId: userId }
+    })
+
+    console.log(theReview)
+    // theReview = theReview.toJSON()
+    // console.log(theReview)
+    // if(theReview.userId !== userId){
+    //     return res.json("Review must belong to the current user")
+    // }
+
+    if(theReview){
+        const theEdit = await ReviewImage.create({
+            url
+        })
+
+        if(theReview.length > 10){
+            res.status(403)
+            return res.json({
+                "message": "Maximum number of images for this resource was reached",
+                "statusCode": 403
+            })
+        }
+
+        return res.json({"id": theEdit.id, "url": url})
+    } else{
+        res.status(404)
+        return res.json({
+            "message": "Review couldn't be found",
+            "statusCode": 404
+        })
+    }
+}) // needs proper error for > 10 images
+
+router.put('/:reviewId', requireAuth, async (req, res) => {
+    const { review, stars } = req.body
+    const { reviewId } = req.params
+    const theReview = await Review.findByPk(reviewId)
+    const { user } = req
+    const userId = user.toSafeObject().id
+
+
+    // console.log(theReview)
+
+    if(!review){
+        res.status(400)
+        return res.json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": { 'review': 'Review text is required.'}
+           })
+        }
+
+    if(stars !== 1 && stars !== 2 && stars !== 3 && stars !== 4 && stars !== 5){
+        res.status(400)
+        return res.json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": { 'stars': 'Stars must be an integer from 1 to 5'}
+           })
+    }
+
+    if(theReview.dataValues.userId !== userId){
+        return res.json("Review must belong to the current user.")
+    }
+
 
 })
 
