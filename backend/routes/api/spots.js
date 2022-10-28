4b29a9483d78871b03508e6a7a2f6cefe7396e9b
zswanson92 <zackta92@gmail.com>
2022-10-28 13:01:22 -0500

    decimal limitation test

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -34,7 +34,7 @@ router.get('/', async (req, res) => {
         let avgRating = await Review.findAll({
             raw: true,
             where: { spotId: aSpot[i].id},
-            attributes: [[Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']]
+            attributes: [[Sequelize.fn('AVG', Sequelize.col('stars'),2), 'avgRating']]
         })
 
         let previewImage = await SpotImage.findAll({
@@ -71,7 +71,7 @@ router.get('/current', restoreUser, requireAuth, async (req, res) => {
     let avgRating = await Review.findAll({
         raw: true,
         where: { spotId: specificSpot[i].id},
-        attributes: [[Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']]
+        attributes: [[Sequelize.fn('AVG', Sequelize.col('stars'), 2), 'avgRating']]
     })
 
     let spotImage = await SpotImage.findAll({
@@ -107,7 +107,7 @@ router.get('/:spotId', async (req, res) => {
         let avgRating = await Review.findAll({
             raw: true,
             where: { spotId: theSpot.id},
-            attributes: [[Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']]
+            attributes: [[Sequelize.fn('AVG', Sequelize.col('stars'),2), 'avgRating']]
         })
 
         let numReviews = await Review.findAll({
