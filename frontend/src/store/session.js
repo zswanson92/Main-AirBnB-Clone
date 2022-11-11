87b22d0027673190615f72629c463be53584307c
zswanson92 <zackta92@gmail.com>
2022-11-11 15:19:05 -0600

    half way thru phase 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,62 @@
+import { csrfFetch } from './csrf';
+
+// pojo action creators
+export const SET = 'session/SET'
+export const REMOVE = 'session/REMOVE'
+
+
+// actions
+export const setSessionUser = (inputParam) => {
+    return {
+        type: SET,
+        payload: inputParam
+    }
+}
+
+export const removeSessionUser = () => {
+    return {
+        type: REMOVE
+    }
+}
+
+// thunk
+export const resSetUser = (user) => async dispatch => {
+    const { credential, password } = user
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+    // if(response.ok){
+        const theUser = await response.json()
+        dispatch(setSessionUser(theUser))
+        return response
+    // }
+}
+
+
+let initialState = {
+    user: null
+}
+
+// REDUCER
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch(action.type){
+        case SET:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default sessionReducer;
