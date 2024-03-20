6da23c4945f081da4a5f2fc985ff9a479443445f
zswan <68711430+zswanson92@users.noreply.github.com>
2024-03-20 14:44:45 -0700

    removing booking

--- frontend/src/components/CreateBooking/CreateBooking.js
+++ frontend/src/components/CreateBooking/CreateBooking.js
@@ -1,143 +1,143 @@
-import './CreateBooking.css'
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from 'react-router-dom'
-import * as bookingActions from '../../store/bookings'
-import * as spotActions from '../../store/spots'
-import { getBookingsThunk } from '../../store/bookings';
-// import { Modal } from '../../context/Modal'
-import LoginForm from '../LoginFormModal/LoginForm';
+// import './CreateBooking.css'
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from 'react-router-dom'
+// import * as bookingActions from '../../store/bookings'
+// import * as spotActions from '../../store/spots'
+// import { getBookingsThunk } from '../../store/bookings';
+// // import { Modal } from '../../context/Modal'
+// import LoginForm from '../LoginFormModal/LoginForm';
 
 
-function CreateBookingButton() {
-  const dispatch = useDispatch();
-  const { spotId } = useParams()
+// function CreateBookingButton() {
+//   const dispatch = useDispatch();
+//   const { spotId } = useParams()
 
-  const currBooking = useSelector(state => state.bookings?.allBookings?.Bookings)
+//   const currBooking = useSelector(state => state.bookings?.allBookings?.Bookings)
 
-  // console.log("CURR BOOKING", currBooking)
+//   // console.log("CURR BOOKING", currBooking)
 
-  const sessionUser = useSelector(state => state.session.user);
+//   const sessionUser = useSelector(state => state.session.user);
 
-  const [startDate, setStartDate] = useState("")
-  const [endDate, setEndDate] = useState("")
-  const [errors, setErrors] = useState([])
+//   const [startDate, setStartDate] = useState("")
+//   const [endDate, setEndDate] = useState("")
+//   const [errors, setErrors] = useState([])
 
-  const [showReviewForm, setReviewForm] = useState(false)
+//   const [showReviewForm, setReviewForm] = useState(false)
 
-  const spotDetailsObj = useSelector(state => {
-    return state.spots.spot[spotId]
-  })
+//   const spotDetailsObj = useSelector(state => {
+//     return state.spots.spot[spotId]
+//   })
 
-  // console.log("create bookings spot details", spotDetailsObj)
-
-
-  useEffect(() => {
-    let err = []
-    if (dateCheck(startDate) || dateCheck(endDate)) {
-      err.push("This date range is already booked. Please choose a different one.")
-    }
-
-    setErrors(err)
-  }, [startDate, endDate])
-
-  const createNewBooking = async (e) => {
-    e.preventDefault();
-
-    if (!sessionUser) {
-      return setErrors(['Must be logged in to make a booking.'])
-    }
-    if (sessionUser?.id === spotDetailsObj?.ownerId) {
-      return setErrors(['You cannot create a booking for a location you own.'])
-    }
+//   // console.log("create bookings spot details", spotDetailsObj)
 
 
+//   useEffect(() => {
+//     let err = []
+//     if (dateCheck(startDate) || dateCheck(endDate)) {
+//       err.push("This date range is already booked. Please choose a different one.")
+//     }
 
-    const createdBooking = {
-      startDate, endDate, spotId
-    }
+//     setErrors(err)
+//   }, [startDate, endDate])
 
-    await dispatch(bookingActions.createBookingThunk(createdBooking))
-    // await dispatch(reviewActions.getAllReviews(spotId))
-    await dispatch(spotActions.getSpotById(spotId))
-    await dispatch(getBookingsThunk(spotId))
-    setStartDate("")
-    setEndDate("")
-    setReviewForm(false)
-  }
+//   const createNewBooking = async (e) => {
+//     e.preventDefault();
 
-  const dateCheck = (date) => {
-
-    let abc = currBooking?.filter((el) => {
-      // console.log("THIS IS DATE!!!", date)
-      // console.log("THIS IS EL!!!", el.startDate.slice(0, 10))
-
-      return date === el.startDate.slice(0, 10) || (date > el.startDate.slice(0, 10) && date < el.endDate.slice(0, 10)) || (date === el.endDate.slice(0, 10))
-
-    })
-    console.log("ABC", abc)
-    if (abc.length > 0) {
-      return true
-    }
-    return false
-  }
-
-  // console.log("START DATE", startDate)
-  // console.log("END DATE", endDate)
-  // console.log("DIFF", endDate - startDate)
+//     if (!sessionUser) {
+//       return setErrors(['Must be logged in to make a booking.'])
+//     }
+//     if (sessionUser?.id === spotDetailsObj?.ownerId) {
+//       return setErrors(['You cannot create a booking for a location you own.'])
+//     }
 
 
-  return (
-    <>
-      {/* <button onClick={() => setReviewForm(true)} className='createbooking-button'> Create a booking</button> */}
-      {
-        // showReviewForm ? <Modal>
 
-        <form onSubmit={createNewBooking} className="new-booking-form">
-          <div className='test-div-create-book'>
-            <div className='enter-date-div'>
-              CHECK-IN
-              <input
-                className='date-inputs'
-                type="date"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-                required={true} />
-            </div>
-            <div className='enter-date-div'>
-              CHECKOUT
-              <input
-                className='date-inputs'
-                type="date"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-                required={true} />
-            </div>
-          </div>
-          {(dateCheck(startDate) || dateCheck(endDate)) ? <div>This date range is already booked. Please choose a different one.</div> : <div> &nbsp; </div>}
-          <button disabled={(dateCheck(startDate) || dateCheck(endDate)) ? true : false} type="submit" className="submitbooking-button">Reserve</button>
-          {/* <button onClick={() => setReviewForm(false)} className='discardreviewform-button'>Close Form</button> */}
-          {errors.length > 0 && (
-            <div className="createbooking-errors-div">
-              {errors.map((error, idx) => (
-                <div key={idx}>{error}</div>
-              ))}
-            </div>
-          )}
-        </form>
+//     const createdBooking = {
+//       startDate, endDate, spotId
+//     }
 
-      }
-    </>
-  )
-}
+//     await dispatch(bookingActions.createBookingThunk(createdBooking))
+//     // await dispatch(reviewActions.getAllReviews(spotId))
+//     await dispatch(spotActions.getSpotById(spotId))
+//     await dispatch(getBookingsThunk(spotId))
+//     setStartDate("")
+//     setEndDate("")
+//     setReviewForm(false)
+//   }
 
-export default CreateBookingButton
+//   const dateCheck = (date) => {
+
+//     let abc = currBooking?.filter((el) => {
+//       // console.log("THIS IS DATE!!!", date)
+//       // console.log("THIS IS EL!!!", el.startDate.slice(0, 10))
+
+//       return date === el.startDate.slice(0, 10) || (date > el.startDate.slice(0, 10) && date < el.endDate.slice(0, 10)) || (date === el.endDate.slice(0, 10))
+
+//     })
+//     console.log("ABC", abc)
+//     if (abc.length > 0) {
+//       return true
+//     }
+//     return false
+//   }
+
+//   // console.log("START DATE", startDate)
+//   // console.log("END DATE", endDate)
+//   // console.log("DIFF", endDate - startDate)
 
 
-{/* </Modal> : "" */ }
+//   return (
+//     <>
+//       {/* <button onClick={() => setReviewForm(true)} className='createbooking-button'> Create a booking</button> */}
+//       {
+//         // showReviewForm ? <Modal>
 
-// {errors.length > 0 && (
-//   <div className="signup-errors-div">
-//     <div>Must be logged in to make a booking.</div>
-//   </div>
-// )}
+//         <form onSubmit={createNewBooking} className="new-booking-form">
+//           <div className='test-div-create-book'>
+//             <div className='enter-date-div'>
+//               CHECK-IN
+//               <input
+//                 className='date-inputs'
+//                 type="date"
+//                 value={startDate}
+//                 onChange={(e) => setStartDate(e.target.value)}
+//                 required={true} />
+//             </div>
+//             <div className='enter-date-div'>
+//               CHECKOUT
+//               <input
+//                 className='date-inputs'
+//                 type="date"
+//                 value={endDate}
+//                 onChange={(e) => setEndDate(e.target.value)}
+//                 required={true} />
+//             </div>
+//           </div>
+//           {(dateCheck(startDate) || dateCheck(endDate)) ? <div>This date range is already booked. Please choose a different one.</div> : <div> &nbsp; </div>}
+//           <button disabled={(dateCheck(startDate) || dateCheck(endDate)) ? true : false} type="submit" className="submitbooking-button">Reserve</button>
+//           {/* <button onClick={() => setReviewForm(false)} className='discardreviewform-button'>Close Form</button> */}
+//           {errors.length > 0 && (
+//             <div className="createbooking-errors-div">
+//               {errors.map((error, idx) => (
+//                 <div key={idx}>{error}</div>
+//               ))}
+//             </div>
+//           )}
+//         </form>
+
+//       }
+//     </>
+//   )
+// }
+
+// export default CreateBookingButton
+
+
+// {/* </Modal> : "" */ }
+
+// // {errors.length > 0 && (
+// //   <div className="signup-errors-div">
+// //     <div>Must be logged in to make a booking.</div>
+// //   </div>
+// // )}
--- frontend/src/components/EditBooking/EditBooking.js
+++ frontend/src/components/EditBooking/EditBooking.js
@@ -1,106 +1,106 @@
-import './EditBooking.css'
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams, useHistory } from 'react-router-dom'
-import * as bookingActions from '../../store/bookings'
-// import * as spotActions from '../../store/spots'
-// import { getBookingsThunk } from '../../store/bookings';
+// import './EditBooking.css'
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams, useHistory } from 'react-router-dom'
+// import * as bookingActions from '../../store/bookings'
+// // import * as spotActions from '../../store/spots'
+// // import { getBookingsThunk } from '../../store/bookings';
 
 
 
-function EditBookingButton() {
-  let history = useHistory();
-  const dispatch = useDispatch();
-  const { bookingId } = useParams()
+// function EditBookingButton() {
+//   let history = useHistory();
+//   const dispatch = useDispatch();
+//   const { bookingId } = useParams()
 
-  const currBooking = useSelector(state => state.bookings?.allBookings.Bookings)
-  // console.log("CURR BOOKING", currBooking)
+//   const currBooking = useSelector(state => state.bookings?.allBookings.Bookings)
+//   // console.log("CURR BOOKING", currBooking)
 
-  const bookingFilter = currBooking?.filter(obj => obj.id === +bookingId)
+//   const bookingFilter = currBooking?.filter(obj => obj.id === +bookingId)
 
-  let editValOne;
-  let editValTwo;
+//   let editValOne;
+//   let editValTwo;
 
-  const workAround = bookingFilter ? editValOne = bookingFilter[0]?.startDate : ""
-  const workAroundTwo = bookingFilter ? editValTwo = bookingFilter[0]?.endDate : ""
-
-
-  const [startDate, setStartDate] = useState(editValOne ? editValOne.slice(0, 10) : "")
-  const [endDate, setEndDate] = useState(editValTwo ? editValTwo.slice(0, 10) : "")
-  const [errors, setErrors] = useState([])
-
-  // const [showReviewForm, setReviewForm] = useState(false)
-
-  useEffect(() => {
-    let err = []
-    if(editDateCheck(startDate) || editDateCheck(endDate)){
-      err.push("This date range is already booked. Please choose a different one.")
-    }
-
-    setErrors(err)
-  }, [startDate, endDate])
+//   const workAround = bookingFilter ? editValOne = bookingFilter[0]?.startDate : ""
+//   const workAroundTwo = bookingFilter ? editValTwo = bookingFilter[0]?.endDate : ""
 
 
-  const editBooking = async (e) => {
-    e.preventDefault();
+//   const [startDate, setStartDate] = useState(editValOne ? editValOne.slice(0, 10) : "")
+//   const [endDate, setEndDate] = useState(editValTwo ? editValTwo.slice(0, 10) : "")
+//   const [errors, setErrors] = useState([])
 
-    const editedBooking = {
-      startDate, endDate
-    }
+//   // const [showReviewForm, setReviewForm] = useState(false)
 
-    await dispatch(bookingActions.editBookingThunk(bookingId, editedBooking))
+//   useEffect(() => {
+//     let err = []
+//     if(editDateCheck(startDate) || editDateCheck(endDate)){
+//       err.push("This date range is already booked. Please choose a different one.")
+//     }
 
-    history.goBack()
-  }
+//     setErrors(err)
+//   }, [startDate, endDate])
 
-  const goBack = (e) => {
-    e.preventDefault();
 
-    history.goBack()
-  }
+//   const editBooking = async (e) => {
+//     e.preventDefault();
 
-  const editDateCheck = (date) => {
+//     const editedBooking = {
+//       startDate, endDate
+//     }
 
-    let abc = currBooking.filter((el) => {
-      // console.log("THIS IS DATE!!!", date)
-      // console.log("THIS IS EL!!!", el.startDate.slice(0, 10))
+//     await dispatch(bookingActions.editBookingThunk(bookingId, editedBooking))
 
-      return date === el.startDate.slice(0, 10) || (date > el.startDate.slice(0, 10) && date < el.endDate.slice(0 ,10)) || (date === el.endDate.slice(0, 10))
+//     history.goBack()
+//   }
 
-    })
-    // console.log("ABC", abc)
-    if(abc.length > 0){
-      return true
-    }
-    return false
-  }
+//   const goBack = (e) => {
+//     e.preventDefault();
 
-  return (
-    <>
-      <form onSubmit={editBooking} className="edit-booking-form">
-        <div className='edit-booking-div'>
-          Enter start date:
-          <input
-            type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            required={true} />
-        </div>
-        <div className='edit-booking-div'>
-          Enter end date:
-          <input
-            type="date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-            required={true} />
+//     history.goBack()
+//   }
 
-        </div>
-        {errors.length > 0 ? <div>This date range is already booked. Please choose a different one.</div> : <div> &nbsp; </div>}
-        <button disabled={(editDateCheck(startDate) || editDateCheck(endDate)) ? true : false} type="submit" className="submitreview-button">Submit edited booking request</button>
-        <button onClick={goBack} className='discardreviewform-button'>Return to location</button>
-      </form>
-    </>
-  )
-}
+//   const editDateCheck = (date) => {
 
-export default EditBookingButton
+//     let abc = currBooking.filter((el) => {
+//       // console.log("THIS IS DATE!!!", date)
+//       // console.log("THIS IS EL!!!", el.startDate.slice(0, 10))
+
+//       return date === el.startDate.slice(0, 10) || (date > el.startDate.slice(0, 10) && date < el.endDate.slice(0 ,10)) || (date === el.endDate.slice(0, 10))
+
+//     })
+//     // console.log("ABC", abc)
+//     if(abc.length > 0){
+//       return true
+//     }
+//     return false
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={editBooking} className="edit-booking-form">
+//         <div className='edit-booking-div'>
+//           Enter start date:
+//           <input
+//             type="date"
+//             value={startDate}
+//             onChange={(e) => setStartDate(e.target.value)}
+//             required={true} />
+//         </div>
+//         <div className='edit-booking-div'>
+//           Enter end date:
+//           <input
+//             type="date"
+//             value={endDate}
+//             onChange={(e) => setEndDate(e.target.value)}
+//             required={true} />
+
+//         </div>
+//         {errors.length > 0 ? <div>This date range is already booked. Please choose a different one.</div> : <div> &nbsp; </div>}
+//         <button disabled={(editDateCheck(startDate) || editDateCheck(endDate)) ? true : false} type="submit" className="submitreview-button">Submit edited booking request</button>
+//         <button onClick={goBack} className='discardreviewform-button'>Return to location</button>
+//       </form>
+//     </>
+//   )
+// }
+
+// export default EditBookingButton
